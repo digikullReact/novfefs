724aaaea09f1f20cd5448a528c2498bc7c6be7fa
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-01-07 20:06:55 +0530

    added

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,17 @@ import React,{useState} from 'react'
 
 function Home() {
     const [state,setState]=useState("monospace")
+    const [i,setI]=useState(0)
 
     // Try a react application with buttons and texts ,so that when you click buttons 
     // it changes the color and fonts 
+    //
+const fontfamily=["cursive","Fantasy","Times New Roman"]
 
     const changeFontFamily=()=>{
-        setState("cursive")
+        setI(i+1)
+        
+        setState(fontfamily[i]);
 
     }
     return (
