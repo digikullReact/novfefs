7740aa0946458a692cc0a24c4471a08910662bda
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-01-07 20:02:12 +0530

    added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Home() {
+    const [state,setState]=useState("monospace")
+
+    // Try a react application with buttons and texts ,so that when you click buttons 
+    // it changes the color and fonts 
+
+    const changeFontFamily=()=>{
+        setState("cursive")
+
+    }
     return (
         <div>
-            <h1>React ,Here I arrive</h1>
+            <h1 style={{fontFamily:state}}>React ,Here I arrive</h1>
+
+            <button onClick={changeFontFamily}>
+                Change Font
+            </button>
             
         </div>
     )
